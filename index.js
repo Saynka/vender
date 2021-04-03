d26be07d4e8c198919b1189462e6667a6a14dfc2
Saynka <sankakale@gmail.com>
2021-04-03 14:09:39 -0700

    thing

--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const io = require('socket.io-client');
-const host = 'http://localhost:3000/caps';
-const socket = io.connect(host);
-socket.emit('start');
-
-
--- vender.js
+++ vender.js
@@ -1,13 +1,19 @@
 'use strict';
-// require('../index.js');
 
-const socket = require('../index.js');
+const io = require('socket.io-client');
+const host = 'http://localhost:3000/caps';
 const faker = require('faker');
 const socket = io.connect(host);
 const venderSocket = io.connect(`${host}/caps`);
+require('dotenv').config();
 
-socket.on('start', newOrder);
-socket.on('delivered', delievered);
+
+venderSocket.emit('start');
+
+
+venderSocket.on('start', newOrder);
+venderSocket.on('delivered', delievered);
+
 
 function newOrder() {
   setInterval(() => {
@@ -33,7 +39,14 @@ function delievered(payload) {
   console.log(`PACKAGE DELIEVERED ${payload.orderId} ON ${new Date()}`)
 }
 
+
+
+
 module.exports = {
   start: newOrder,
   delievered: delievered
 }
+
+
+
+
